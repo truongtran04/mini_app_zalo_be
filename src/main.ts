885afd2c10d69dd2fc52939da650504d98d7312e
truongtran04 <trtr2k4@gmail.com>
2026-05-19 13:17:15 +0700

    chore: update environment configurations for render deployment

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 5000);
+  const port = process.env.PORT ?? 5000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
